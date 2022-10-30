3308f0877ded241d6b699a4cf249c9ece0e85525
MuhyiSandyTsalasa <116779544+MuhyiSandyTsalasa@users.noreply.github.com>
2022-10-30 12:34:24 +0700

    add full code page

---
+++ script.js
@@ -0,0 +1,54 @@
+/* bahan bahan image
+https://hajatans.com/photo/mitra/logo/165258083616002.png
+https://hajatans.com/partner-theme/default/images/preview1.webp
+https://hajatans.com/partner-theme/default/images/preview2.webp
+https://hajatans.com/partner-theme/default/images/preview3.webp
+
+https://seomuda.id/wp-content/uploads/2020/10/png-images-for-website-background-picture-406390-png-images-for-website-background-png-1024_640-570x320.png
+https://hajatans.com/partner-theme/default/images/images-about-app.webp
+https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQNVWhs4C4wMNd3jX-wcJ4KfWBieYl4SPgLQ&usqp=CAU
+*/
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top" id="mainNav">
+            <div class="container">
+            <a class="navbar-brand navbar-expand" href="#">Menu</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link js-scroll-trigger" href="#">Home <span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#">Packages</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Product
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Undangan Pernikahan</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Undangan Video</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Undangan Khitanan</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Undangan Aqiqah</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Undangan Wisuda</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Undangan Birthday</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#">Register</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav> 
